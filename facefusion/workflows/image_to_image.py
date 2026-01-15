8ddca612d1b2ceb25f25e285d08ab30852391618
johnson <1660390816@qq.com>
2026-01-15 15:16:19 +0800

    remove nsfw&make gradio listen 0.0.0.0

--- facefusion/workflows/image_to_image.py
+++ facefusion/workflows/image_to_image.py
@@ -3,7 +3,7 @@ from functools import partial
 from facefusion import ffmpeg
 from facefusion import logger, process_manager, state_manager, translator
 from facefusion.audio import create_empty_audio_frame
-from facefusion.content_analyser import analyse_image
+# from facefusion.content_analyser import analyse_image  # NSFW检测已禁用
 from facefusion.filesystem import is_image
 from facefusion.processors.core import get_processors_modules
 from facefusion.temp_helper import clear_temp_directory, create_temp_directory, get_temp_file_path
@@ -35,8 +35,9 @@ def process(start_time : float) -> ErrorCode:
 
 
 def setup() -> ErrorCode:
-	if analyse_image(state_manager.get_item('target_path')):
-		return 3
+	# NSFW检测已禁用
+	# if analyse_image(state_manager.get_item('target_path')):
+	# 	return 3
 
 	logger.debug(translator.get('clearing_temp'), __name__)
 	clear_temp_directory(state_manager.get_item('target_path'))
--- facefusion/workflows/image_to_video.py
+++ facefusion/workflows/image_to_video.py
@@ -8,7 +8,7 @@ from facefusion import ffmpeg
 from facefusion import logger, process_manager, state_manager, translator, video_manager
 from facefusion.audio import create_empty_audio_frame, get_audio_frame, get_voice_frame
 from facefusion.common_helper import get_first
-from facefusion.content_analyser import analyse_video
+# from facefusion.content_analyser import analyse_video  # NSFW检测已禁用
 from facefusion.filesystem import filter_audio_paths, is_video
 from facefusion.processors.core import get_processors_modules
 from facefusion.temp_helper import clear_temp_directory, create_temp_directory, move_temp_file, resolve_temp_frame_paths
@@ -44,8 +44,9 @@ def process(start_time : float) -> ErrorCode:
 def setup() -> ErrorCode:
 	trim_frame_start, trim_frame_end = restrict_trim_frame(state_manager.get_item('target_path'), state_manager.get_item('trim_frame_start'), state_manager.get_item('trim_frame_end'))
 
-	if analyse_video(state_manager.get_item('target_path'), trim_frame_start, trim_frame_end):
-		return 3
+	# NSFW检测已禁用
+	# if analyse_video(state_manager.get_item('target_path'), trim_frame_start, trim_frame_end):
+	# 	return 3
 
 	logger.debug(translator.get('clearing_temp'), __name__)
 	clear_temp_directory(state_manager.get_item('target_path'))
